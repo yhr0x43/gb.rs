2b76d8bbff37dc9ca3450f46db07ddd91de4a6de
Timothy Pizarro <timothypi314@gmail.com>
2026-03-01 03:49:52 -0600

    Deleted joypad.rs and incorporated it into bus.rs

--- src/bus.rs
+++ src/bus.rs
@@ -7,6 +7,12 @@ pub struct Bus {
     vram: [u8; 0x2000], 
     wram: [u8; 0x2000],
     apu: audio::Apu,
+
+    joy_p1: u8,
+    action_buttons: u8, // A, B, Select, Start. 0 = Pressed; 1 = Not pressed.
+    direction_buttons: u8, // Right, Left, Up, Down. 0 = Pressed; 1 = Not pressed.
+
+
 }
 
 impl Bus {
@@ -16,6 +22,9 @@ impl Bus {
             vram: [0; 0x2000],
             wram: [0; 0x2000],
             apu: audio::Apu::new(),
+            joy_p1: 0xFF, 
+            action_buttons: 0x0F,
+            direction_buttons: 0x0F,
         }
     }
 
@@ -27,9 +36,15 @@ impl Bus {
             0x8000..0xA000 => self.vram[uaddr - 0x8000],
             0xC000..0xE000 => self.wram[uaddr - 0xC000],
             0xFF10..0xFF40 => self.apu.readByte(addr),
+            0xFF00 => {
+                let mut result = self.joy_p1 | 0xC0; // Upper 2 bits always read as 1
+                if self.joy_p1 & 0x10 == 0 { result &= self.action_buttons; }
+                if self.joy_p1 & 0x20 == 0 { result &= self.direction_buttons; }
+                result
+                }
+            }
             _ => todo!("memory address read {:04X}", addr),
         }
-    }
 
     pub fn write(&mut self, addr: Addr, val: u8) {
         let uaddr: usize = addr.into();
@@ -37,7 +52,12 @@ impl Bus {
             0x8000..0xA000 => self.vram[uaddr - 0x8000] = val,
             0xC000..0xE000 => self.wram[uaddr - 0xC000] = val,
             0xFF10..0xFF40 => self.apu.writeByte(addr, val),
+            0xFF00 => self.joy_p1 = (val & 0x30) | 0xC0, 
             _ => todo!("memory address write {:04X}", addr),
         };
     }
 }
+
+
+
+
